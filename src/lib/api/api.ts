8f0a3c8b3ac9ef6a25a4b54a192880da57410367
Judith Yusuf <judithyusuf00@gmail.com>
2025-06-07 02:11:13 +0100

    fix:removed credentials

--- src/lib/api/api.ts
+++ src/lib/api/api.ts
@@ -4,7 +4,6 @@ const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 const api = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
